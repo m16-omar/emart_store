d260f6191d319e9f6d9393de2ad6a2d9cdbb5d1c
Umar Abdullahi <68796054+m16-omar@users.noreply.github.com>
2024-09-20 05:31:11 +0100

    Add files via upload

--- lib/src/onboarding/views/widgets/welcome_screen.dart
+++ lib/src/onboarding/views/widgets/welcome_screen.dart
@@ -1,4 +1,6 @@
 import 'package:emart_store/common/utils/kcolors.dart';
+import 'package:emart_store/common/utils/kstrings.dart';
+import 'package:emart_store/common/widgets/app_style.dart';
 import 'package:emart_store/const/resource.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -18,7 +20,18 @@ class WelcomeScreen extends StatelessWidget {
             SizedBox(
               height: 100.h,
             ),
-            Image.asset(R.ASSETS_IMAGES_GETSTARTED_PNG)
+            Image.asset(R.ASSETS_IMAGES_GETSTARTED_PNG),
+            SizedBox(
+              height: 30.h,
+            ),
+            Text(
+              AppText.kWelcomeHeader,
+              textAlign: TextAlign.center,
+              style: appStyle(24, Kolors.kPrimary, FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30.h,
+            ),
           ],
         ),
       ),
