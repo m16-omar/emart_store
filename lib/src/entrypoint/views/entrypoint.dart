2d1fb204fa408694235f95f30e68e859e09281d3
Umar Abdullahi <68796054+m16-omar@users.noreply.github.com>
2024-09-21 13:48:04 +0100

    Add files via upload

--- lib/src/entrypoint/views/entrypoint.dart
+++ lib/src/entrypoint/views/entrypoint.dart
@@ -1,10 +1,25 @@
+import 'package:emart_store/src/cart/views/cart_screen.dart';
+import 'package:emart_store/src/home/views/home_screen.dart';
+import 'package:emart_store/src/profile/views/profile_screen.dart';
+import 'package:emart_store/src/wishlist/views/wishlist_screen.dart';
 import 'package:flutter/material.dart';
 
 class AppEntryPoint extends StatelessWidget {
-  const AppEntryPoint({super.key});
+  AppEntryPoint({super.key});
+
+  List<Widget> pageList = [
+    const HomeScreen(),
+    const WishlistScreen(),
+    const CartScreen(),
+    const ProfileScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [pageList[0]],
+      ),
+    );
   }
 }
