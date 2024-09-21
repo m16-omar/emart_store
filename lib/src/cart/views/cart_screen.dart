39ecd41a30f45fad0a0fe6384c821e126617c9f4
Umar Abdullahi <68796054+m16-omar@users.noreply.github.com>
2024-09-21 13:51:14 +0100

    Add files via upload

--- lib/src/cart/views/cart_screen.dart
+++ lib/src/cart/views/cart_screen.dart
@@ -5,6 +5,10 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text("Cart Screen"),
+      ),
+    );
   }
 }
--- lib/src/wishlist/views/wishlist_screen.dart
+++ lib/src/wishlist/views/wishlist_screen.dart
@@ -5,6 +5,10 @@ class WishlistScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text("Wishlist Screen"),
+      ),
+    );
   }
 }
