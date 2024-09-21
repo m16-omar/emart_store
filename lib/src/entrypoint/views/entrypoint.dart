c5df95f8a232989c459470758277e7a0035f3451
Umar Abdullahi <68796054+m16-omar@users.noreply.github.com>
2024-09-21 15:21:06 +0100

    Add files via upload

--- lib/src/entrypoint/views/entrypoint.dart
+++ lib/src/entrypoint/views/entrypoint.dart
@@ -1,8 +1,10 @@
+import 'package:emart_store/common/utils/kcolors.dart';
 import 'package:emart_store/src/cart/views/cart_screen.dart';
 import 'package:emart_store/src/home/views/home_screen.dart';
 import 'package:emart_store/src/profile/views/profile_screen.dart';
 import 'package:emart_store/src/wishlist/views/wishlist_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 
 class AppEntryPoint extends StatelessWidget {
   AppEntryPoint({super.key});
@@ -18,7 +20,53 @@ class AppEntryPoint extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: [pageList[0]],
+        children: [
+          pageList[3],
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Theme(
+              data: Theme.of(context).copyWith(canvasColor: Kolors.kOffWhite),
+              child: BottomNavigationBar(
+                selectedFontSize: 12,
+                elevation: 0,
+                backgroundColor: Kolors.kOffWhite,
+                showSelectedLabels: true,
+                showUnselectedLabels: false,
+                unselectedIconTheme: IconThemeData(color: Colors.black38),
+                items: [
+                  BottomNavigationBarItem(
+                      icon: Icon(
+                        AntDesign.home,
+                        color: Kolors.kPrimary,
+                        size: 24,
+                      ),
+                      label: "Home"),
+                  BottomNavigationBarItem(
+                      icon: Icon(
+                        AntDesign.home,
+                        color: Kolors.kPrimary,
+                        size: 24,
+                      ),
+                      label: "Wishlist"),
+                  BottomNavigationBarItem(
+                      icon: Icon(
+                        AntDesign.home,
+                        color: Kolors.kPrimary,
+                        size: 24,
+                      ),
+                      label: "Cart"),
+                  BottomNavigationBarItem(
+                      icon: Icon(
+                        AntDesign.home,
+                        color: Kolors.kPrimary,
+                        size: 24,
+                      ),
+                      label: "Profile"),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
