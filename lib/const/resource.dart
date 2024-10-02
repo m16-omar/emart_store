b31fb2d3de6f3a44137e6f1b74a6f425d771ff57
Umar Abdullahi <68796054+m16-omar@users.noreply.github.com>
2024-10-02 15:05:23 +0100

    Add files via upload

--- lib/const/resource.dart
+++ lib/const/resource.dart
@@ -4,37 +4,37 @@
 class R {
   const R._();
 
-  /// ![preview](file:///Volumes/Transfer/Projects/fashion/.env.development)
+  /// ![preview](file:///Volumes/Transfer/Projects/emart_store/.env.development)
   static const String _ENV_DEVELOPMENT = '.env.development';
 
-  /// ![preview](file:///Volumes/Transfer/Projects/fashion/.env.production)
+  /// ![preview](file:///Volumes/Transfer/Projects/emart_store/.env.production)
   static const String _ENV_PRODUCTION = '.env.production';
 
-  /// ![preview](file:///Volumes/Transfer/Projects/fashion/assets/images/checkout.png)
+  /// ![preview](file:///Volumes/Transfer/Projects/emart_store/assets/images/checkout.png)
   static const String ASSETS_IMAGES_CHECKOUT_PNG = 'assets/images/checkout.png';
 
-  /// ![preview](file:///Volumes/Transfer/Projects/fashion/assets/images/empty.png)
+  /// ![preview](file:///Volumes/Transfer/Projects/emart_store/assets/images/empty.png)
   static const String ASSETS_IMAGES_EMPTY_PNG = 'assets/images/empty.png';
 
-  /// ![preview](file:///Volumes/Transfer/Projects/fashion/assets/images/experience.png)
+  /// ![preview](file:///Volumes/Transfer/Projects/emart_store/assets/images/experience.png)
   static const String ASSETS_IMAGES_EXPERIENCE_PNG =
       'assets/images/experience.png';
 
-  /// ![preview](file:///Volumes/Transfer/Projects/fashion/assets/images/failed.png)
+  /// ![preview](file:///Volumes/Transfer/Projects/emart_store/assets/images/failed.png)
   static const String ASSETS_IMAGES_FAILED_PNG = 'assets/images/failed.png';
 
-  /// ![preview](file:///Volumes/Transfer/Projects/fashion/assets/images/getstarted.png)
+  /// ![preview](file:///Volumes/Transfer/Projects/emart_store/assets/images/getstarted.png)
   static const String ASSETS_IMAGES_GETSTARTED_PNG =
       'assets/images/getstarted.png';
 
-  /// ![preview](file:///Volumes/Transfer/Projects/fashion/assets/images/placeholder.webp)
+  /// ![preview](file:///Volumes/Transfer/Projects/emart_store/assets/images/placeholder.webp)
   static const String ASSETS_IMAGES_PLACEHOLDER1_WEBP =
       'assets/images/placeholder1.webp';
 
-  /// ![preview](file:///Volumes/Transfer/Projects/fashion/assets/images/splashscreen.png)
+  /// ![preview](file:///Volumes/Transfer/Projects/emart_store/assets/images/splashscreen.png)
   static const String ASSETS_IMAGES_SPLASHSCREEN_PNG =
       'assets/images/splashscreen.png';
 
-  /// ![preview](file:///Volumes/Transfer/Projects/fashion/assets/images/wishlist.png)
+  /// ![preview](file:///Volumes/Transfer/Projects/emart_store/assets/images/wishlist.png)
   static const String ASSETS_IMAGES_WISHLIST_PNG = 'assets/images/wishlist.png';
 }
