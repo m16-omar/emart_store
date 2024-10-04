3c6b7d6847c1b22148713581dba65d0e6160599a
Umar Abdullahi <68796054+m16-omar@users.noreply.github.com>
2024-10-04 22:41:15 +0100

    Add files via upload

--- lib/src/home/widgets/home_tabs.dart
+++ lib/src/home/widgets/home_tabs.dart
@@ -1,39 +1,39 @@
+import 'package:emart_store/common/utils/kcolors.dart';
+import 'package:emart_store/common/widgets/app_style.dart';
+import 'package:emart_store/common/widgets/tab_widget.dart';
+import 'package:emart_store/src/home/views/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-// import 'package:emart_store/common/utils/kcolors.dart';
-// import 'package:emart_store/common/widgets/app_style.dart';
-// import 'package:emart_store/common/widgets/tab_widget.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
+class HomeTabs extends StatelessWidget {
+  const HomeTabs({super.key, required TabController tabController})
+      : _tabController = tabController;
 
-// class HomeTabs extends StatelessWidget {
-//   const HomeTabs({super.key, required TabController tabController})
-//       : _tabController = tabController;
+  final TabController _tabController;
 
-//   final TabController _tabController;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SizedBox(
-//       height: 22.h,
-//       child: TabBar(
-//         controller: _tabController,
-//         indicator: BoxDecoration(
-//             color: Kolors.kPrimary, borderRadius: BorderRadius.circular(25)),
-//         labelPadding: EdgeInsets.zero,
-//         labelColor: Kolors.kWhite,
-//         dividerColor: Colors.transparent,
-//         tabAlignment: TabAlignment.start,
-//         isScrollable: true,
-//         labelStyle: appStyle(11, Kolors.kPrimary, FontWeight.w600),
-//         unselectedLabelStyle: appStyle(11, Kolors.kGray, FontWeight.normal),
-//         tabs: List.generate(
-//             homeTabs.length,
-//             (i) => Tab(
-//                   child: TabWidget(
-//                     text: homeTabs[i],
-//                   ),
-//                 )),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 22.h,
+      child: TabBar(
+        controller: _tabController,
+        indicator: BoxDecoration(
+            color: Kolors.kPrimary, borderRadius: BorderRadius.circular(25)),
+        labelPadding: EdgeInsets.zero,
+        labelColor: Kolors.kWhite,
+        dividerColor: Colors.transparent,
+        tabAlignment: TabAlignment.start,
+        isScrollable: true,
+        labelStyle: appStyle(11, Kolors.kPrimary, FontWeight.w600),
+        unselectedLabelStyle: appStyle(11, Kolors.kGray, FontWeight.normal),
+        tabs: List.generate(
+            homeTabs.length,
+            (i) => Tab(
+                  child: TabWidget(
+                    text: homeTabs[i],
+                  ),
+                )),
+      ),
+    );
+  }
+}
