7eb2b65505767637f3d743391aa7d9d0d2b8347d
Umar Abdullahi <68796054+m16-omar@users.noreply.github.com>
2024-10-05 21:52:36 +0100

    Add files via upload

--- lib/src/wishlist/views/wishlist_screen.dart
+++ lib/src/wishlist/views/wishlist_screen.dart
@@ -1,13 +1,24 @@
+
+import 'package:fashion_app/common/utils/kcolors.dart';
+import 'package:fashion_app/common/utils/kstrings.dart';
+import 'package:fashion_app/common/widgets/app_style.dart';
+import 'package:fashion_app/common/widgets/reusable_text.dart';
+import 'package:fashion_app/src/products/widgets/explore_products.dart';
 import 'package:flutter/material.dart';
 
-class WishlistScreen extends StatelessWidget {
-  const WishlistScreen({super.key});
+class WishListPage extends StatelessWidget {
+  const WishListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Wishlist Screen"),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: ReusableText(text: AppText.kWishlist, style: appStyle(16, Kolors.kPrimary, FontWeight.bold)),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: ExploreProducts(),
       ),
     );
   }
